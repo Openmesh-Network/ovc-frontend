54c40f4f181703456db4890fac11c9aff117eb19
Plopmenz <plopmenz@gmail.com>
2024-05-20 17:57:17 +0200

    indexer update

--- config/wagmi-config.ts
+++ config/wagmi-config.ts
@@ -1,14 +1,14 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 import { cookieStorage, createStorage } from "wagmi";
-import { arbitrumSepolia, sepolia } from "wagmi/chains";
+import { polygon, mainnet } from "wagmi/chains";
 
 import { siteConfig } from "./site";
 import { createPublicClient, http } from "viem";
 
 export const appName = siteConfig.name;
 export const appDescription = siteConfig.description;
-export const appIcon = "https://ovc.plopmenz.com/icon.png" as const;
-export const appUrl = "https://ovc.plopmenz.com" as const;
+export const appIcon = "https://ovc.openmesh.network/icon.png" as const;
+export const appUrl = "https://ovc.openmesh.network" as const;
 const metadata = {
   name: appName,
   description: appDescription,
@@ -17,7 +17,7 @@ const metadata = {
 };
 
 export const projectId = "0ec5e8af894898c29bc27a1c4dc11e78" as const;
-export const chains = [arbitrumSepolia] as const;
+export const chains = [polygon] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
@@ -28,13 +28,13 @@ export const config = defaultWagmiConfig({
   }),
 });
 
-export const defaultChain = arbitrumSepolia;
+export const defaultChain = polygon;
 export const defaultPublicClient = createPublicClient({
   chain: defaultChain,
   transport: http(),
 });
 
-export const crosschainAccountChain = sepolia;
+export const crosschainAccountChain = mainnet;
 export const crosschainAccountPublicClient = createPublicClient({
   chain: crosschainAccountChain,
   transport: http(),
